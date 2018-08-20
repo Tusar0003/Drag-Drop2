a4f81d31d3da6252c46c12f23690ae9f14313d8f
Tusar0003 <faisalahmedtusar@gmail.com>
2018-08-21 00:05:33 +0600

    Initial Commit

---
+++ app/src/main/java/com/example/no0ne/dragdrop2/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.no0ne.dragdrop2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private ViewGroup rootLayout;
+    private int xDelta, yDelta;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rootLayout = (ViewGroup) findViewById(R.id.rootView);
+        imageView = (ImageView) rootLayout.findViewById(R.id.imageView);
+
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 150);
+        imageView.setLayoutParams(layoutParams);
+        imageView.setOnTouchListener(new ChoiceTouchListener());
+    }
+
+    private final class ChoiceTouchListener implements View.OnTouchListener {
+        @Override
+        public boolean onTouch(View view, MotionEvent event) {
+            final int X = (int) event.getRawX();
+            final int Y = (int) event.getRawY();
+            switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                case MotionEvent.ACTION_DOWN:
+                    RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                    xDelta = X - lParams.leftMargin;
+                    yDelta = Y - lParams.topMargin;
+                    break;
+                case MotionEvent.ACTION_UP:
+                    break;
+                case MotionEvent.ACTION_POINTER_DOWN:
+                    break;
+                case MotionEvent.ACTION_POINTER_UP:
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
+                            .getLayoutParams();
+                    layoutParams.leftMargin = X - xDelta;
+                    layoutParams.topMargin = Y - yDelta;
+                    layoutParams.rightMargin = -250;
+                    layoutParams.bottomMargin = -250;
+                    view.setLayoutParams(layoutParams);
+                    break;
+            }
+            rootLayout.invalidate();
+
+            return true;
+        }
+    }
+}
